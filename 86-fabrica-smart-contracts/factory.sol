5aa17b944c3777d6be3b09e41ed17b3361332fbe
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2024-05-04 13:08:05 +0200

    Fix variables

--- 86-fabrica-smart-contracts/factory.sol
+++ 86-fabrica-smart-contracts/factory.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.4;
 
 contract padre {
-    mapping(address => address) personal_contract;
+    mapping(address => address) public personal_contract;
 
     function Factory() public {
         address addr_personal_contract = address(
             new hijo(msg.sender, address(this))
         );
+        personal_contract[msg.sender] = addr_personal_contract;
     }
 }
 
