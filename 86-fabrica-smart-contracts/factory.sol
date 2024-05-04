91b5c601ce9d1e7ebd9d97288204c2d32bbb0d6e
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2024-05-04 13:06:11 +0200

    fix variable names

--- 86-fabrica-smart-contracts/factory.sol
+++ 86-fabrica-smart-contracts/factory.sol
@@ -2,20 +2,22 @@
 pragma solidity ^0.8.4;
 
 contract padre {
-    mapping (address => address) personal_contract;
+    mapping(address => address) personal_contract;
 
     function Factory() public {
-        address addr_personal_contract = address (new hijo(msg.sender, address(this)));
+        address addr_personal_contract = address(
+            new hijo(msg.sender, address(this))
+        );
     }
 }
 
 contract hijo {
-    string Owner {
+    struct Owner {
         address _owner;
         address _smartContractPadre;
     }
 
-    Owner public propiertario;
+    Owner public propietario;
 
     constructor(address _account, address _accountSC) {
         propietario._owner = _account;
