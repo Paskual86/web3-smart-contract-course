044e567e786952c2f74b78a2c1c28cdabb52a88e
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2024-05-08 06:22:44 +0200

    Fix some issues

--- 92-creacion-token-erc20/ERC20.sol
+++ 92-creacion-token-erc20/ERC20.sol
@@ -81,7 +81,7 @@ contract ERC20 is IERC20 {
     function allowance(
         address owner,
         address spender
-    ) public virtual override returns (bool) {
+    ) public view virtual override returns (uint256) {
         return _allowances[owner][spender];
     }
 
@@ -130,14 +130,21 @@ contract ERC20 is IERC20 {
         return true;
     }
 
-    function _transfer(address from, address to, uint256 amount) internal virtual {
-        require(from!=address(0), "ERC20: transfer from zero address");
-        require(to!=address(0), "ERC20: transfer to zero address");
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {
+        require(from != address(0), "ERC20: transfer from zero address");
+        require(to != address(0), "ERC20: transfer to zero address");
 
         _beforeTokenTransfer(from, to, amount);
 
         uint256 fromBalance = _balances[from];
-        require(fromBalance>=amount, "ERC20: transfer amount exceeds balance");
+        require(
+            fromBalance >= amount,
+            "ERC20: transfer amount exceeds balance"
+        );
         unchecked {
             _balances[from] = fromBalance - amount;
         }
@@ -150,10 +157,10 @@ contract ERC20 is IERC20 {
     function _mint(address account, uint amount) internal virtual {
         require(account != address(0), "ERC20: mint to the zero address");
         _beforeTokenTransfer(address(0), account, amount);
-        _totalSuply +=amount;
+        _totalSuply += amount;
         _balances[account] += amount;
         emit Transfer(address(0), account, amount);
-        _afterTokenTransfer(address(0), account, amount)
+        _afterTokenTransfer(address(0), account, amount);
     }
 
     function _burn(address account, uint256 amount) internal virtual {
@@ -162,39 +169,57 @@ contract ERC20 is IERC20 {
 
         uint accountBalance = _balances[account];
 
-        require(accountBalance>=amount, "ERC20: burn amount exceeds balance");
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
 
         unchecked {
             _balances[account] = accountBalance - amount;
         }
         _totalSuply -= amount;
         emit Transfer(account, address(0), amount);
-        _afterTokenTransfer(account, address(0),  amount);
+        _afterTokenTransfer(account, address(0), amount);
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
         _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, value);
+        emit Approval(owner, spender, amount);
     }
 
-    function _spendAllowance(address owner, address spender, uint256 amount) internal virtual {
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
         uint256 currentAllowance = allowance(owner, spender);
         if (currentAllowance != type(uint256).max) {
-            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+            require(
+                currentAllowance >= amount,
+                "ERC20: insufficient allowance"
+            );
             unchecked {
                 _approve(owner, spender, amount);
             }
         }
     }
 
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {
         // Estas funciones se dejan vacias para modificar el comportamiento en el caso de que hereden desde nuestro smart contracts
     }
-    
-    function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual {
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {
         // Estas funciones se dejan vacias para modificar el comportamiento en el caso de que hereden desde nuestro smart contracts
     }
 }
