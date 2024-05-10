0868dfe4e8afb5c13e6a909cc8c80871b7fb0830
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2024-05-10 07:32:36 +0200

    add first NFT contract

--- 14-tokens-ERC-20/107-generacion-token-erc721/customERC721.sol
+++ 14-tokens-ERC-20/107-generacion-token-erc721/customERC721.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.4;
 // Importaciones
 // Recordar establecer las versiones
 import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.5.0/token/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract erc721 is ERC721 {
     using Counters for Counters.Counter;
@@ -17,7 +17,7 @@ contract erc721 is ERC721 {
     ) ERC721(_name, _symbol) {}
 
     // Esto solo genera un registro del activo que estoy queriendo enviar
-    function sendNFT(address account) public {
+    function sendNFT(address _account) public {
         _tokensIds.increment();
         uint256 newItemId = _tokensIds.current();
         _safeMint(_account, newItemId);
